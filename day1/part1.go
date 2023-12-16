2815b2287575078435549a2e79a49dea0886c738
easterok <beskrovnyk@gmail.com>
2023-12-16 12:40:46 +0300

    aoc 2022 day 1 part 1

--- day1/part1.go
+++ day1/part1.go
@@ -1,5 +1,26 @@
 package day1
 
+import (
+	"aoc/utils"
+	"strings"
+)
+
 func Part1(content string) int {
-	return 0
+	elves := strings.Split(content, "\r\n\r\n")
+
+	result := 0
+
+	for _, elf := range elves {
+		calories := strings.Split(elf, "\r\n")
+
+		c := 0
+
+		for _, k := range calories {
+			c += utils.LineToNums(k)[0]
+		}
+
+		result = max(c, result)
+	}
+
+	return result
 }
